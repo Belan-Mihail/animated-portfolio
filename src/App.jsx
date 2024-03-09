d7e4afe35e30fcd3a4b0cfe0a2f83b961e244723
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-03-09 21:03:07 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+    </div>
+  );
 };
 
 export default App;
+
+//1 install sass, create app.scss add styles create components folder and navbar.jsx and scss
+//add navbar to app.jsx
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+// import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      {/* <Sidebar/> */}
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Developper Name
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
+//2 sidebar later. add basic html and css
+//3 install framer-motion (npm i framer-motion@10.16.4) 
+//4 animate span with name
+
+// 32:00
